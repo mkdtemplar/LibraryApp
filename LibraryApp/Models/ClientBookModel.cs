1ab1d5486dd91dc67fe8f991fc98bfbb62c37b0b
Ivan Markovski <sagitariusim@gmail.com>
2021-09-30 01:16:24 +0200

    09/29/21

---
+++ LibraryApp/Models/ClientBookModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using LibraryData;
+
+namespace LibraryApp.Models
+{
+    public class ClientBookModel
+    {
+        public IList<Book> Books { get; set; }
+        public IList<Client> Clients { get; set; }
+    }
+}
