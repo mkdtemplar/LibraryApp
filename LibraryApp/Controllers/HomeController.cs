bcf5314abdb258a73a76d71557d24f6b6fc5c01f
Ivan Markovski <sagitariusim@gmail.com>
2021-09-29 22:34:11 +0200

    09/29/21

--- LibraryApp/Controllers/HomeController.cs
+++ LibraryApp/Controllers/HomeController.cs
@@ -26,11 +26,7 @@ namespace LibraryApp.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
